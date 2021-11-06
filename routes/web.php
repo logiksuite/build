d38e7ec9c01b34983839cda72df37e331e6ae02c
Benoit Gauthier <bgauthier@globalia.ca>
2021-11-06 11:47:17 -0400

    Added prefixes to routes

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,19 @@ use LogikSuite\Build\LogikBuild;
 |
 */
 
-Route::get('/', function () {
-    return view('ui');
+Route::groupe(['prefix','logikbuild'], function () {
+
+    Route::get('/', function () {
+        return view('logikbuild:ui');
+    });
+
+
+    Route::get('/translation/language', [TranslationController::class, 'languages']);
+    Route::get('/translation/editor/{file?}', [TranslationController::class, 'editor']);
+    Route::get('/translation/get/{file}/{key}/{locale}', [TranslationController::class, 'get']);
+    Route::post('/translation/get', [TranslationController::class, 'getBatch']);
+    Route::post('/translation/locale', [TranslationController::class, 'addLocale']);
+    Route::delete('/translation/locale', [TranslationController::class, 'removeLocale']);
+    Route::post('/translation/translate', [TranslationController::class, 'translate']);
+    Route::post('/translation/save', [TranslationController::class, 'save']);
 });
-
-
-Route::get('/translation/language', [TranslationController::class, 'languages']);
-
-Route::get('/translation/editor/{file?}', [TranslationController::class, 'editor']);
-Route::get('/translation/get/{file}/{key}/{locale}', [TranslationController::class, 'get']);
-Route::post('/translation/get', [TranslationController::class, 'getBatch']);
-
-Route::post('/translation/locale', [TranslationController::class, 'addLocale']);
-Route::delete('/translation/locale', [TranslationController::class, 'removeLocale']);
-
-Route::post('/translation/translate', [TranslationController::class, 'translate']);
-
-Route::post('/translation/save', [TranslationController::class, 'save']);
